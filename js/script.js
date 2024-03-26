d8b1375c7f71a5292f0c2b0a28f6c64b64408328
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-03-26 14:48:31 -0600

    Set logic for followup math operations

--- js/script.js
+++ js/script.js
@@ -13,23 +13,33 @@ for (const n of numBtn){
     n.addEventListener('click', function(n){
         n.stopImmediatePropagation;
         console.log("clicked" + this.id);
-        if (operator === undefined && firstNum == 0){
+        
+        if (solved && operator == undefined){
+            solved = false;
             firstNum = this.id;
             display.textContent = firstNum;
         }
-        else if (firstNum != 0 && operator === undefined){
-            firstNum = firstNum.concat(this.id);
-            display.textContent = firstNum;
-        }
-        else if (secondNum === undefined){
-            secondNum = this.id;
-            display.textContent = firstNum.concat(operator,secondNum);
-        }
         else{
-            secondNum = secondNum + this.id;
-            display.textContent = firstNum.concat(operator,secondNum);
+            if (operator === undefined && firstNum == 0){
+                firstNum = this.id;
+                display.textContent = firstNum;
+            }
+            else if (firstNum != 0 && operator === undefined){
+                firstNum = firstNum.concat(this.id);
+                display.textContent = firstNum;
+            }
+            else if (secondNum === undefined){
+                secondNum = this.id;
+                display.textContent = firstNum.concat(operator,secondNum);
+            }
+            else{
+                secondNum = secondNum + this.id;
+                display.textContent = firstNum.concat(operator,secondNum);
+            }
         }
-       
+
+    
+    
     }, false);
 }
 
